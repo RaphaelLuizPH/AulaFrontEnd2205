d68cd2170ed35c473cf3e5b2c6119f75fda26afb
RaphaelLuizPH <130625114+RaphaelLuizPH@users.noreply.github.com>
2024-06-05 16:50:25 -0300

    Animations

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 AOS.init();
+
+function start() {
+  const content = document.getElementById("section1");
+
+  content.scrollIntoView({ block: "center", behavior: "smooth" });
+}
+
+function buttonanimate() {
+  let butt = document.getElementById("buttoncenter");
+  butt.classList.toggle("close");
+}
